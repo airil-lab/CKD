9f9fe02c331dcd0be83d86748e69aa8709d1666e
airil-lab <office.airil@gmail.com>
2025-05-03 09:20:25 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,10 +25,10 @@ rc = st.number_input("Red Cells / RBC Count", min_value=2, max_value=8, value=6,
 sc = st.number_input("Serum Creatinine", min_value=0.4, max_value=76.1, step=1.1, value=35.1)
 sg = st.number_input("Specific Gravity of Urine", min_value=1.1, max_value=1.2, step=0.1, value=1.1)
 bgr = st.number_input("Blood Glucose Random", min_value=22, max_value=490, step=1, value=250)
-al = st.number_input("Albumin", min_value=0, max_value=5.1, step=0.1, value=2.5)
+al = st.number_input("Albumin", min_value=0.0, max_value=5.1, step=0.1, value=2.5)
 sod = st.number_input("Sodium - Na⁺", min_value=104, max_value=163, step=1, value=130)
-pot = st.number_input("Potassium - K⁺", min_value=2.5, max_value=47, step=1.1, value=30.1)
-bu = st.number_input("Blood Urea", min_value=1.5, max_value=391, step=1.1, value=150.1)
+pot = st.number_input("Potassium - K⁺", min_value=2.5, max_value=47.1, step=1.1, value=30.1)
+bu = st.number_input("Blood Urea", min_value=1.5, max_value=391.1, step=1.1, value=150.1)
 
 
 # Collect all inputs in a list
