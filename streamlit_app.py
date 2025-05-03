bb91237cf390dc790cdbba3cb3dd889305808cc1
airil-lab <office.airil@gmail.com>
2025-05-03 09:17:48 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ Fill in the fields below and click *Predict*.
 hemo = st.number_input("Hemoglobin", min_value=3.1, max_value=17.8, step=1.1, value=10.1)
 pcv = st.number_input("Packed Cell Volume / Hematocrit", min_value=9, max_value=54, value=30, step=1)
 rc = st.number_input("Red Cells / RBC Count", min_value=2, max_value=8, value=6, step=1)
-sc = st.number_input("Serum Creatinine", min_value=0.4, max_value=76, step=1.1, value=35.1)
+sc = st.number_input("Serum Creatinine", min_value=0.4, max_value=76.1, step=1.1, value=35.1)
 sg = st.number_input("Specific Gravity of Urine", min_value=1, max_value=1.2, step=0.1, value=1.1)
 bgr = st.number_input("Blood Glucose Random", min_value=22, max_value=490, step=1, value=250)
 al = st.number_input("Albumin", min_value=0, max_value=5.1, step=0.1, value=2.5)
